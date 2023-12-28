c4f39c9f60d609b64e8061fcdd0175cd49b3d11d
Lazizbek Rakhmatov <lazizrakhmat@gmail.com>
2023-12-28 09:10:36 +0500

    added dataset from Udemy

---
+++ 92-110.sql
@@ -0,0 +1,67 @@
+#SELECT 
+#	*
+#FROM 
+#	salaries
+#WHERE 
+#	salary BETWEEN 66.000 AND 70.000 ;
+    
+#SELECT 
+#	*
+#FROM
+#	employees
+#WHERE 
+#	emp_no NOT BETWEEN '10004' AND '10012' ;
+
+#SELECT 
+#	dept_name
+#FROM 
+#	departments
+#WHERE 
+#	dept_no IS NOT NULL ;
+
+#SELECT 
+#	*
+#FROM 
+#	employees
+#WHERE 
+#	hire_date >= '2000-01-01' AND gender = 'F' ;
+
+SELECT 
+	* 
+FROM 
+	salaries
+WHERE 
+	salary > '150000' ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+    
+
+
+
+
+
+
+
+
+    
+    
+    
+
+
