46c1d6c8661cd568cd519e22fbd0653cb5722b0a
Lazizbek Rakhmatov <lazizrakhmat@gmail.com>
2024-01-15 22:08:21 +0500

    CROSS JOIN

---
+++ CROSS JOIN.sql
@@ -0,0 +1,22 @@
+### CROSS JOIN ###
+
+SELECT 
+	dm.*, d.*
+FROM
+	departments d
+		CROSS JOIN
+	dept_manager dm
+WHERE d.dept_no = 'd009'
+ORDER BY emp_no;
+
+
+SELECT 
+	e.*, d.*
+FROM
+	employees e
+		CROSS JOIN
+	departments d
+WHERE e.emp_no < 10011
+ORDER BY e.emp_no, d.dept_name;
+
+
